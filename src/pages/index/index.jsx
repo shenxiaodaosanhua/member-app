6aa818a68ccc97aa58e77b58ca361a750705b651
神霄三华 <87976956@qq.com>
2020-11-10 01:34:31 +0800

    首页

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,8 +1,11 @@
 import React, { Component } from 'react'
+import Taro from '@tarojs/taro'
 import {
   View,
 } from '@tarojs/components'
 import {
+  AtAvatar,
+  AtDivider,
   AtList,
   AtListItem,
 } from 'taro-ui'
@@ -12,13 +15,57 @@ export default class Index extends Component {
 
 
   render () {
+    let userInfo = Taro.getStorageSync('userInfo')
     return (
       <View>
+        <View className='at-row at-row__justify--center'>
+          <View
+            className='at-col-2'
+          >
+            <AtAvatar
+              image={userInfo.avatarUrl}
+              circle
+              size='large'
+            />
+          </View>
+        </View>
+        <AtDivider
+          content='操作'
+          lineColor='#cccccc'
+          fontColor='#cccccc'
+        />
         <AtList>
-          <AtListItem title='查询进度' arrow='right' />
-          <AtListItem title='宽带报装' arrow='right' />
-          <AtListItem title='故障申报' arrow='right' />
+          <AtListItem
+            title='查询进度'
+            arrow='right'
+            iconInfo={{ color: '#13CE66', value: 'eye', }}
+          />
+          <AtListItem
+            title='宽带报装'
+            arrow='right'
+            iconInfo={{ color: '#13CE66', value: 'add-circle', }}
+          />
+          <AtListItem
+            title='故障申报'
+            arrow='right'
+            iconInfo={{ color: '#FFC82C', value: 'close-circle', }}
+          />
         </AtList>
+        <AtDivider
+          content='个人设置'
+          lineColor='#cccccc'
+          fontColor='#cccccc'
+        />
+        <AtListItem
+          title='绑定小程序'
+          arrow='right'
+          iconInfo={{ color: '#13CE66', value: 'message', }}
+        />
+        <AtListItem
+          title='退出'
+          arrow='right'
+          iconInfo={{ color: '#FFC82C', value: 'trash', }}
+        />
       </View>
     )
   }
